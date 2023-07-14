aa0a018f1b3109df6d87f25046126d1dfd27fc9c
Carlos Pintado <cpintado@github.com>
2023-07-14 22:36:09 +0200

    Update and rename JavaConverter.java to example.py

--- JavaConverter.java
+++
@@ -1,5 +0,0 @@
-public static Object deserialize (InputStream is)
-    throws IOException {
-    ObjectInputStream ois = new ObjectInputStream(is);
-    return ois.readObject();
-}
---
+++ example.py
@@ -0,0 +1,12 @@
+# This is a simple Python script that adds two numbers together
+
+def add(a, b):
+    return a + b
+
+num1 = 5
+num2 = 7
+
+if error: pass
+
+result = add(num1, num2)
+print(result)
